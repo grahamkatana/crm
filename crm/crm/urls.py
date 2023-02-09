b6714f6a7c0afed6b8e360dea17c6f97f4920564
grahamkatana <graykatanakenny@gmail.com>
2023-02-09 16:24:26 +0200

    Simple dashboard

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index,about
 from userprofile.views import signup
 from django.contrib.auth import views
@@ -8,6 +8,7 @@ from django.contrib.auth import views
 urlpatterns = [
     path('',index,name='index'),
     path('signup/',signup, name='signup'),
+    path('dashboard/',include('dashboard.urls')),
     path('log-in/',views.LoginView.as_view(template_name='userprofile/login.html'),name='login'),
     path('log-out/',views.LogoutView.as_view(),name='logout'),
     path('about',about,name='about'),
