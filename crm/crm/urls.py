475c1f0b728c0c6249a12e2004915acbbb0ca94e
grahamkatana <graykatanakenny@gmail.com>
2023-02-09 16:05:12 +0200

    Sign out

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('',index,name='index'),
     path('signup/',signup, name='signup'),
     path('log-in/',views.LoginView.as_view(template_name='userprofile/login.html'),name='login'),
+    path('log-out/',views.LogoutView.as_view(),name='logout'),
     path('about',about,name='about'),
     path('admin/', admin.site.urls),
 ]
